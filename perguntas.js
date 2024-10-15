d69d35c251b8e5c4bf049af7f29cfb824eaaff12
aliciacontadoraa <00001130014721sp@al.educacao.sp.gov.br>
2024-10-15 12:41:44 -0300

    projeto final

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemnha é Berlim'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz Coração em Inglês?',
+    'Coração em ingles é Heart'
+)
+criaCartao(
+    'História',
+    'Quando ocorreu a ditadura no Brasil?',
+    'A ditadura ocorreu em 1964 a 1985'
+)
+
+criaCartao(
+    'Português',
+    'O que é um verbo?',
+    'Verbo indica ação'
+)
